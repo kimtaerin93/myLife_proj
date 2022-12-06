64e4625a122a8c5295cd802fd131884a9c01af54
taerin.k <kimtaerin@doub.co.kr>
2022-12-06 15:28:23 +0900

    template 적용

--- myLife/src/main/java/com/taerin/myLife/main/controller/MainController.java
+++ myLife/src/main/java/com/taerin/myLife/main/controller/MainController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class MainController {
 
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
 	@RequestMapping("/test")
 	public String main() {
 		return "test";
